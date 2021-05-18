c1aafc20184279e8702fd501bc12b6f4c677c9e2
Shamsiddinkhon <43116863+Shamsiddinkhon@users.noreply.github.com>
2021-05-18 21:51:06 +0500

    18.05.2021
    NOde js 3 lesson

--- IT_STEP/Vue.js/Lesson_2/index.js
+++ IT_STEP/Vue.js/Lesson_2/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -35,18 +35,21 @@ const comments = [
 app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/comments", (req, res) => {
+app.get("/comments123", (req, res) => {
   res.render("comments/comments", { comments });
 });
+
 app.get("/comments/new", (req, res) => {
   res.render("comments/new", { comments });
 });
+
+
 //getting new comment
 app.post("/comments", (req, res) => {
-//   console.log(req.body);
-  const { username, comment, id=comments.length+1} = req.body
-  comments.push({ username, comment, id})
-  res.redirect("/comments")
+  //   console.log(req.body);
+  const { username, comment, id = comments.length + 1 } = req.body;
+  comments.push({ username, comment, id });
+  res.redirect("/comments");
 });
 
 app.listen(3000, () => {
---
+++ IT_STEP/Vue.js/Lesson_3/New/views/partials/Lesson_2_Continue/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+
+const { v4: uuid } = require("uuid");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+const comments = [
+  {
+    id: uuid(),
+    username: "John",
+    comment: "Hello my names is John",
+  },
+  {
+    id: uuid(),
+    username: "Shams",
+    comment: "Hello my names is John",
+  },
+  {
+    id: uuid(),
+    username: "Mike",
+    comment: "Hello my names is John",
+  },
+  {
+    id: uuid(),
+    username: "Sam",
+    comment: "Hello my names is John",
+  },
+  {
+    id: uuid(),
+    username: "Hi",
+    comment: "Hello my names is John",
+  },
+];
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/comments", (req, res) => {
+  res.render("comments/comments", { comments });
+});
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new", { comments });
+});
+// Getting ID
+app.get("/comments/:id", (req, res) => {
+  console.log(req.params);
+  // perevodim stoku na chislo
+  const { id } = req.params;
+  console.log(id);
+  const comment = comments.find((c) => c.id === id);
+  console.log(comment);
+  res.render("comments/show", { comment });
+});
+//getting new comment
+app.post("/comments", (req, res) => {
+  //   console.log(req.body);
+  const { username, comment, id = uuid()} = req.body;
+  comments.push({ username, comment, id });
+  res.redirect("/comments");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
